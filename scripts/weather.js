557fff233f04c528f56b4dc6547e098ac138c1a4
ldumlao1204 <leilany.dumlao@gmail.com>
2026-01-29 00:03:18 +0800

    edited weasther.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -3,4 +3,29 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/___________';
+// Create Required Variables for the URL
+const myKey = "350390bbf1828b6f3c0decce2bdf19c9"
+const myLat = "49.749466013479534"
+const myLong = "6.638127831468744"
+
+// Construct a Full Path USing Template Literals
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}`
+
+// Try to grab the Current Weather Data
+async function apiFetch() {
+    try {
+        const response = await fetch(myURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            // displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
