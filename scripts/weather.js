43c381666fc8d9c1cd0f27aebe16958849d2708d
ldumlao1204 <leilany.dumlao@gmail.com>
2026-01-29 17:23:59 +0800

    completed weather activity

--- scripts/weather.js
+++ scripts/weather.js
@@ -8,8 +8,8 @@ const myKey = "350390bbf1828b6f3c0decce2bdf19c9"
 const myLat = "49.749466013479534"
 const myLong = "6.638127831468744"
 
-// Construct a Full Path USing Template Literals
-const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}`
+// Construct a Full Path Using Template Literals
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
 
 // Try to grab the Current Weather Data
 async function apiFetch() {
@@ -17,8 +17,8 @@ async function apiFetch() {
         const response = await fetch(myURL);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // testing only
-            // displayResults(data); // uncomment when ready
+            console.log(data); 
+            displayResults(data); 
         } else {
             throw Error(await response.text());
         }
@@ -27,5 +27,16 @@ async function apiFetch() {
     }
 }
 
+// Display the JSON DATA onto the web page    
+function displayResults(data) {
+    console.log('hello')
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', icon);
+    weatherIcon.setAttribute('alt', data.weather[0].icon)
+    captionDesc.textContent = `${desc}`;
+}
+    
 apiFetch();
 
