6a4652968495e0d1e0d72aba15bdc8b3e46fe1f9
ldumlao1204 <leilany.dumlao@gmail.com>
2026-02-07 14:13:25 +0800

    udpated siteplan.css and getdates.js

--- final/scripts/getdates.js
+++ final/scripts/getdates.js
@@ -1,9 +1,7 @@
 // Set current year
 const currentYearSpan = document.getElementById('currentyear');
-const currentYear = new Date().getFullYear();
-currentYearSpan.textContent = currentYear;
+currentYearSpan.textContent = new Date().getFullYear();
 
 // Set last modified date
-const lastModifiedParagraph = document.getElementById('lastModified');
-const lastModified = document.lastModified;
-lastModifiedParagraph.textContent = `Last Modified: ${lastModified}`;
+const lastModifiedSpan = document.getElementById('last-modified');
+lastModifiedSpan.textContent = document.lastModified;
